e47de836e3828797cc2696c0ab6b93ae53f77fbc
ChannhNghia <chanhnghia@gmail.gmailcom>
2026-01-15 11:10:52 +0700

    update scence victory và chuyển scence

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,12 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    void LoadScenePlayGame()
+    public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+      
+        SceneManager.LoadScene("GamePlay");
     }
-    void GameExit()
+    public void BackToMenu()
     {
-        Application.Quit();
+       
+        SceneManager.LoadScene("StartGame");
+    }
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void NextLevel()
+    {   
+        int currentLevel = PlayerPrefs.GetInt("CurrentLevel", 1);      
+        currentLevel++;       
+        PlayerPrefs.SetInt("CurrentLevel", currentLevel);
+        PlayerPrefs.Save(); 
+
+        Debug.Log("Đã chuyển sang Level: " + currentLevel);
+
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+        
+        Debug.Log("Đã bấm nút thoát game!");
     }
 }
